d0dd5e6bb924ed3dae97da679c7dc8f931389a0c
gem install <gatesoul@gmail.com>
2017-11-22 00:10:09 -0300

    computers index view

---
+++ app/controllers/computers_controller.rb
@@ -0,0 +1,5 @@
+class ComputersController < ApplicationController
+
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resource :computers
+  resources :computers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
